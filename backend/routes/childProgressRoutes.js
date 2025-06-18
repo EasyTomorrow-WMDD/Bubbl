e021fa6b86947a45b61e89b1974d041f6b7ab14d
Andres Castaneda <yoac@Andress-MacBook-Air.local>
2025-06-18 12:59:26 -0700

    Code merged with AUTH logic

--- backend/routes/childProgressRoutes.js
+++ backend/routes/childProgressRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/childProgressController');
-// const auth = require('../middleware/auth');
 
-router.get('/dashboard', /*auth*/ controller.getDashboard);
+
+router.get('/dashboard/:userId', controller.getDashboard);
 router.get('/modules', controller.getAllModulesAndTopics);
 router.get('/userProgress', controller.getAllModulesAndTopics);
 
