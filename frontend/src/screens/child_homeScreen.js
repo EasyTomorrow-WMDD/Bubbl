1a4dbe819c475a8330f9134ca0ec974c636151cb
Andres Castañeda <andres.castaeda97@gmail.com>
2025-06-18 00:06:18 -0700

    Child Home Screen Layout

--- frontend/src/screens/child_homeScreen.js
+++ frontend/src/screens/child_homeScreen.js
@@ -1,21 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import StatsPanel from '../components/containers/StatCards';
+import Module from '../components/containers/ModulesDashboard';
 
 export default function ChildHome() {
+  const [user, setUser] = useState(null);
+  const [modules, setModules] = useState([]);
+  console.log('These are the modules', modules)
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/childProgress/dashboard')
+      .then(response => setUser(response.data))
+      .catch(error => console.error('Error fetching user data:', error));
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/childProgress/modules')
+      .then(response => setModules(response.data))
+      .catch(error => console.error('Error fetching user data:', error));
+  }, [])
+
   return (
-    <View style={styles.container}>
-      
-      <StatusBar style="auto" />
+    <View >
+      <ScrollView contentContainerStyle={{ paddingBottom: 140 }}>
+        <View style={{ flex: 1, gap: 10 }}>
+          <StatusBar style="auto" />
+          <ImageBackground
+            source={require('../assets/images/Background_Purple.png')}
+            resizeMode="cover"
+            style={styles.background}
+          >
+            <View style={styles.container}>
+              <Image source={require('../assets/images/yellow_bubbl.png')} style={styles.img} />
+              <Text style={styles.title}>Welcome, {user ? user.user_nickname : '...'}!</Text>
+              <StatsPanel user={user} />
+              <Text style={styles.text}>Next HP refill in:</Text>
+            </View>
+          </ImageBackground>
+
+          <View style={styles.cardContainer}>
+            <Module modules={modules} />
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.playTopic}>
+        <Text>Keep Playing</Text>
+      </View>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    width: '100%',
+    overflow: 'hidden',
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    padding: 0,
+    margin: 0
   },
+
+  container: {
+    alignItems: 'center',
+    padding: 30,
+    gap: 10,
+  },
+  img: {
+    height: 190,
+    width: 140,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  subHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cardContainer: {
+    width: '100%',
+    paddingTop: 30,
+    paddingHorizontal: 30,
+    flexDirection: 'column',
+    gap: 10,
+    alignItems: 'flex-start',
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+  },
+
+  playTopic: {
+    backgroundColor: '#FFC670',
+    width: '95%',
+    height: 90,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 160
+  }
+
 });
