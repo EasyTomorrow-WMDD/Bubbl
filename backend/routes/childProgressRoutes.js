6b45eff7b889cd5347d91e928fe0cbda3a4da834
Andres Castaneda <yoac@Andress-MacBook-Air.local>
2025-06-18 00:06:18 -0700

    Child Home Screen Layout

--- backend/routes/childProgressRoutes.js
+++ backend/routes/childProgressRoutes.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/childProgressController');
 // const auth = require('../middleware/auth');
 
 router.get('/dashboard', /*auth*/ controller.getDashboard);
+router.get('/modules', controller.getAllModulesAndTopics);
+router.get('/userProgress', controller.getAllModulesAndTopics);
 
 module.exports = router;
