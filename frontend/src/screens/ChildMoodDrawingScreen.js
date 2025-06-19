5da492879e70010f01a1b517550f91ce47785aaa
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-06-18 17:37:30 -0700

    Test compability Dependencies of drawing

---
+++ frontend/src/screens/ChildMoodDrawingScreen.js
@@ -0,0 +1,7 @@
+import ChildMoodDrawingContainer from '../components/containers/ChildMoodDrawingContainer';
+
+const ChildMoodDrawingScreen = ({navigation}) => {
+  return ( <ChildMoodDrawingContainer navigation={navigation} /> );
+};
+
+export default ChildMoodDrawingScreen;
