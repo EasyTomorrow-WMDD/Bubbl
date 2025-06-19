3d0bcb7f3a9adcefb46e092ff77465c9c73a2c80
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-06-18 17:37:30 -0700

    Test compability Dependencies of drawing

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
 const dashboardRoutes = require('./routes/childProgressRoutes');
 const userRoutes = require('./routes/userRoutes');
 const storyRoutes = require('./routes/storyRoutes');
+const drawingRoutes = require('./routes/drawingRoutes');
 
 app.use(express.json());
 app.use('/api/childProgress', dashboardRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/stories', storyRoutes);
+app.use('/api/drawings', drawingRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
