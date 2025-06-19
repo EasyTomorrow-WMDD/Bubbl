04fce7774fa98eeff595f812e40c7473c9acd3b3
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-06-18 23:00:16 -0700

    Change Drawing Section to wireframe + connect to supabase to store on folders

--- frontend/src/screens/ChildMoodDrawingScreen.js
+++ frontend/src/screens/ChildMoodDrawingScreen.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import ChildMoodDrawingContainer from '../components/containers/ChildMoodDrawingContainer';
 
-const ChildMoodDrawingScreen = ({navigation}) => {
-  return ( <ChildMoodDrawingContainer navigation={navigation} /> );
+const ChildMoodDrawingScreen = ({ navigation, route }) => {
+  const { childProfileId } = route.params;
+
+  return (
+    <ChildMoodDrawingContainer
+      navigation={navigation}
+      route={{ params: { childProfileId } }}
+    />
+  );
 };
 
 export default ChildMoodDrawingScreen;
