26b08259c912f7f0438d2ef061adbc81bbd38980
Andres Castaneda <yoac@Andress-MacBook-Air.local>
2025-06-21 11:19:29 -0700

    Topic # fixed

---
+++ frontend/src/screens/ShopScreen.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
