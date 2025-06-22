ea1e1dca21b658035fef040fba02fc2366374668
Yasuhiro Hasegawa <yasuhiro.hasegawa@gmail.com>
2025-06-22 02:40:00 -0700

    EZ-129 Parent Stories implemented

--- frontend/src/config/BubblConfig.js
+++ frontend/src/config/BubblConfig.js
@@ -4,17 +4,21 @@ const BubblConfig = {
   // Backend API
   // For development, check the local IP address with `ifconfig` or `ipconfig` command and replace the IP address in the URL with the IP address of localhost.
   BACKEND_URL: isDev
-    ? 'http://10.100.2.107:3000'
+    ? 'http://192.168.1.81:3000' // Local IP address at home
+    // ? 'http://10.128.229.169:3000' // Local IP address in T building
     : 'https://some-url-in-aws-to-be-created.com',
 
   // Supabase URL
   SUPABASE_URL: isDev
-    ? 'https://ogbfgllkkyehcurolhjf.supabase.co'
+    ? 'https://abdkngdxtrszvkwvqkmx.supabase.co'
     : 'https://ogbfgllkkyehcurolhjf.supabase.co',
 
   SUPABASE_ANON_KEY: isDev
-    ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nYmZnbGxra3llaGN1cm9saGpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTA0NTMsImV4cCI6MjA2NDc2NjQ1M30.VIU3Wj-qyaRl4wT9wXGLFflrqooF58-SS8kZq9K8cBU'
+    ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZGtuZ2R4dHJzenZrd3Zxa214Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxODk1ODQsImV4cCI6MjA2NDc2NTU4NH0.9YxOPyYyA61hdOJx5GvuXti7118WI9kxcaO_Mia2PPI'
     : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nYmZnbGxra3llaGN1cm9saGpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTA0NTMsImV4cCI6MjA2NDc2NjQ1M30.VIU3Wj-qyaRl4wT9wXGLFflrqooF58-SS8kZq9K8cBU',
-};
+
+  // Supabase project ID
+  SUPABASE_PROJECT_ID: isDev ? 'abdkngdxtrszvkwvqkmx' : 'ogbfgllkkyehcurolhjf',
+  };
 
 export default BubblConfig;
