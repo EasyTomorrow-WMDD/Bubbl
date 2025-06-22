fe263cec539e62f12f3db7130d4a90816d786587
Yasuhiro Hasegawa <yasuhiro.hasegawa@gmail.com>
2025-06-22 16:25:32 -0700

    Merged with development

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ const drawingRoutes = require('./routes/drawingRoutes');
 const topicsRoutes = require('./routes/topicsRoutes');
 const energyRoutes = require('./routes/energyRoutes');
 const modulesRoutes = require('./routes/modulesRoutes');
-const storyRoutes = require('./routes/storyRoutes');
 
 //Increase img size to upload
 app.use(express.json({ limit: '5mb' }));
@@ -24,8 +23,6 @@ app.use('/api/topics', topicsRoutes);
 app.use('/api/energy', energyRoutes);
 ///////////////Modules/////////////////
 app.use('/api/modules', modulesRoutes);
-
-app.use('/api/stories', storyRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
