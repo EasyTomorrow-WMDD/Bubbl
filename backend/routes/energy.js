29f9b70bbd2a9f6152da21ae1515a850dc091b3c
Francisco J Franco  Jaramillo <83324177+ffranco10@users.noreply.github.com>
2025-06-21 22:34:15 -0700

    Delete backend directory

--- backend/routes/energy.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getEnergyStatus, reduceEnergy } = require('../controllers/energyController');
-
-router.get('/status', getEnergyStatus);
-router.post('/reduce', reduceEnergy);
-
-module.exports = router;
--- backend/routes/modules.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllModules } = require('../controllers/modulesController');
-
-router.get('/', getAllModules);
-
-module.exports = router;
