a17a93f4077b6ebb84408906e76f94354b8b5a8d
Francisco J Franco  Jaramillo <83324177+ffranco10@users.noreply.github.com>
2025-06-21 22:34:25 -0700

    Delete frontend directory

--- frontend/src/screens/ProfileScreen.js
+++
@@ -1,7 +0,0 @@
-import ProfileContainer from '../components/containers/ProfileContainer';
-
-const ProfileScreen = ({navigation}) => {
-  return ( <ProfileContainer navigation={navigation} /> );
-};
-
-export default ProfileScreen;
