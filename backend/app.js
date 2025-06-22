7aa808e90c5195bb27d2e0bf92b375b6d568cd1c
Yasuhiro Hasegawa <yasuhiro.hasegawa@gmail.com>
2025-06-22 02:40:00 -0700

    EZ-129 Parent Stories implemented

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const drawingRoutes = require('./routes/drawingRoutes');
 const topicsRoutes = require('./routes/topicsRoutes');
 const energyRoutes = require('./routes/energyRoutes');
 const modulesRoutes = require('./routes/modulesRoutes');
+const storyRoutes = require('./routes/storyRoutes');
 
 //Increase img size to upload
 app.use(express.json({ limit: '5mb' }));
@@ -24,7 +25,7 @@ app.use('/api/energy', energyRoutes);
 ///////////////Modules/////////////////
 app.use('/api/modules', modulesRoutes);
 
-
+app.use('/api/stories', storyRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
