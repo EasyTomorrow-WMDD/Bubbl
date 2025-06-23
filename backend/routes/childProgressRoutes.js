6869dafdb630e6c6d5642da8c291a3028a114de1
Francisco Franco <franciscofranco@Franciscos-MacBook-Air.local>
2025-06-23 13:25:52 -0700

    WIP: backup before force push to EZ-94_Francisco

--- backend/routes/childProgressRoutes.js
+++ backend/routes/childProgressRoutes.js
@@ -6,5 +6,6 @@ const controller = require('../controllers/childProgressController');
 router.get('/dashboard/:userId', controller.getDashboard);
 router.get('/modules', controller.getAllModulesAndTopics);
 router.get('/userProgress/:userId', controller.getChildProgress);
+router.post('/saveProgress', controller.saveProgress);
 
 module.exports = router;
