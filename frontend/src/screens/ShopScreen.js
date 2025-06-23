3ef99e6e8c655decdef2e15e48be6444d4517fc5
Andres Castaneda <yoac@Andress-MacBook-Air.local>
2025-06-22 18:25:19 -0700

    Inventory screen

--- frontend/src/screens/ShopScreen.js
+++
@@ -1,6 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
