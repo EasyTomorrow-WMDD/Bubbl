7ab9fbff7d11d5c0bf3336911b653821948b9989
Andres Castañeda <andres.castaeda97@gmail.com>
2025-06-22 22:57:06 -0700

    Inventory layout updated

---
+++ frontend/src/components/containers/Avatar.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Pressable } from 'react-native';
+import { useState, useEffect, createContext } from 'react';
+
+const Avatar = ({ user, onPress }) => {
+    return (
+        <Text>Hola Soy Bubbl</Text>
+    )
+}
+
+export default Avatar;
--- frontend/src/screens/InventoryScreen.js
+++ frontend/src/screens/InventoryScreen.js
@@ -1,50 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState, useEffect, createContext } from 'react';
 import axios from 'axios';
 import Header from '../components/layout/Header';
 import { BASE_URL } from '../utils/config';
-import {StatsInventory} from '../components/containers/StatCards';
+import { StatsInventory } from '../components/containers/StatCards';
+import Avatar from '../components/containers/Avatar';
 
 
 
 const InventoryScreen = ({ navigation, route }) => {
-    const { childProfileId } = route.params || {};
-    const [user, setUser] = useState(null);
-    console.log('READING USER', user)
-    useEffect(() => {
-        if (!childProfileId) return;
+  const { childProfileId } = route.params || {};
+  const [user, setUser] = useState(null);
+  const [section, setSection] = useState('assets');
+  console.log('READING USER', user)
+  useEffect(() => {
+    if (!childProfileId) return;
 
-        const fetchUser = async () => {
-            try {
-                const response = await axios.get(`${BASE_URL}/api/childProgress/dashboard/${childProfileId}`);
-                setUser(response.data);
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        };
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/api/childProgress/dashboard/${childProfileId}`);
+        setUser(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
 
-        fetchUser();
-    }, [childProfileId]);
+    fetchUser();
+  }, [childProfileId]);
 
-    if (!user) return <Text>Loading...</Text>;
+  if (!user) return <Text>Loading...</Text>;
 
-    return (
-        <View >
-            <Header />
-            <View>
-                <View style={{alignItems: 'center', marginVertical: 20}}>
-                    <Text style={styles.title}>Inventory</Text>
-                </View>
-                <View>
-                    <StatsInventory user={user} />
-                </View>
-            </View>
-
-
+  return (
+    <View >
+      <Header />
+      <View>
+        <View style={{ alignItems: 'center', marginVertical: 20 }}>
+          <Text style={styles.title}>Inventory</Text>
         </View>
-    )
+        <View>
+          <StatsInventory user={user} />
+        </View>
+      </View>
+      <View style={styles.container}>
+        <Pressable style={styles.text} onPress={() => setSection('assets')}>
+          <Text style={styles.text}>Assets</Text>
+        </Pressable>
+        <Pressable style={styles.text} onPress={() => setSection('badges')}>
+          <Text style={styles.text}>Badges</Text>
+        </Pressable>
+      </View>
+      {section === 'assets' ? <Pressable>
+        <View>
+          <Avatar/>
+        </View>
+      </Pressable> : <Text>Badges</Text>}
+
+
+    </View>
+  )
 }
 
 export default InventoryScreen;
@@ -54,6 +69,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 30,
     gap: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 
   title: {
@@ -68,6 +85,6 @@ const styles = StyleSheet.create({
 
   text: {
     fontSize: 16,
-    color: 'white',
+    color: 'black',
   },
 });
