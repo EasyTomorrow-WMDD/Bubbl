47ebea5a76ba641938febef03c0d55aaa73298a1
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-06-25 16:41:56 -0700

    Add Animations to Mood

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://192.168.1.72:3000';
+const BASE_URL = 'http://10.128.198.35:3000';
 const URI_URL = 'https://ogbfgllkkyehcurolhjf.supabase.co/storage/v1/object/public/assets/Badges'
 
 export { BASE_URL };
@@ -6,4 +6,5 @@ export { URI_URL };
 
 
 // Langara 10.128.230.106
+// Langara-2 10.128.198.35
 // home 192.168.1.72
