da8cd509c256186524cee2553dd243af2061abd2
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-06-24 22:19:32 -0700

    Parent Child Retrieve Drawing Now workin

--- backend/routes/drawingRoutes.js
+++ backend/routes/drawingRoutes.js
@@ -1,3 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const verifySupabaseToken = require('../middleware/verifySupabaseToken');
+// const drawingController = require('../controllers/drawingController');
+
+// router.post('/upload', verifySupabaseToken, drawingController.uploadDrawing);
+// router.get('/retrieve', verifySupabaseToken, drawingController.retrieveDrawings);
+
+// router.get('/:user_profile_id', verifySupabaseToken, drawingController.getDrawingsByUser);
+// router.get('/getByChild/:userId', verifySupabaseToken, drawingController.getDrawingsByChild);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
 
@@ -5,5 +21,9 @@ const verifySupabaseToken = require('../middleware/verifySupabaseToken');
 const drawingController = require('../controllers/drawingController');
 
 router.post('/upload', verifySupabaseToken, drawingController.uploadDrawing);
+
+router.get('/getByChild/:userId', verifySupabaseToken, drawingController.getDrawingsByChild);
+
+// router.get('/list/:userId', verifySupabaseToken, drawingController.listDrawings);
 
 module.exports = router;
