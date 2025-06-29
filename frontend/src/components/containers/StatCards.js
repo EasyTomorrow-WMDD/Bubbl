fb1906c3dbcbf352395f7cd046875b138890065c
Andres Castañeda <andres.castaeda97@gmail.com>
2025-06-29 01:04:57 -0700

    Child Main Screen Styles Updated

--- frontend/src/components/containers/StatCards.js
+++ frontend/src/components/containers/StatCards.js
@@ -5,43 +5,42 @@ import FavoriteBadgesDisplay from './FavoriteBadgesDisplay';
 
 
 const StatCard = ({ label, icon, value, extra, bgHeader, bgColor }) => (
-    <View style={[styles.card, {backgroundColor:bgColor}]}>
-        <View style={[styles.cardHeader, {backgroundColor:bgHeader}]}>
-            <Text style={styles.headerText}>{label}</Text>
-        </View>
-        <Image source={icon} style={styles.icon} />
-        <Text style={styles.valueText}>{value} {extra && <Text style={styles.extraText}>{extra}</Text>}</Text>
+  <View style={{ backgroundColor: bgHeader, borderRadius: 20 }}>
+    <View style={[styles.cardHeader, { backgroundColor: bgHeader }]}>
+      <Text style={styles.headerText}>{label}</Text>
     </View>
+    <View style={[styles.card, { backgroundColor: bgColor }]}>
+      <Image source={icon} style={styles.icon} />
+      <Text style={styles.valueText}>{value} {extra && <Text style={styles.extraText}>{extra}</Text>}</Text>
+    </View>
+  </View>
 );
 
-export default function StatsPanel({user}) {
+export default function StatsPanel({ user }) {
+  if (!user) return null;
+  console.log(user);
 
-    if (!user) return null;
-    console.log(user);
-
-    return (
-        <View style={styles.container}>
-            <StatCard label="Total HP" icon={require('../../assets/icons/heart.png')} value={user.user_energy} bgHeader={'#EE47EB'} bgColor={'#FDE8FF'}/>
-            <StatCard label="Total Stars" icon={require('../../assets/icons/star.png')} value={user.user_star} bgHeader={'#F99707'} bgColor={'#FFF2C6'} />
-            <StatCard label="XP" icon={require('../../assets/icons/flash.png')} value={user.user_xp} bgHeader={'#11BBB8'} bgColor={'#CBFCF6'}/>
-        </View>
-    );
-
+  return (
+    <View style={styles.container}>
+      <StatCard label="Total HP" icon={require('../../assets/icons/heart.png')} value={user.user_energy} bgHeader={'#EE47EB'} bgColor={'#FDE8FF'} />
+      <StatCard label="Total Stars" icon={require('../../assets/icons/star.png')} value={user.user_star} bgHeader={'#F99707'} bgColor={'#FFF2C6'} />
+      <StatCard label="XP" icon={require('../../assets/icons/flash.png')} value={user.user_xp} bgHeader={'#11BBB8'} bgColor={'#CBFCF6'} />
+    </View>
+  );
 }
 
-export function StatsInventory({user, badges}){
-       if (!user) return null;
-    
-    return (
-        <View>
-        <View style={styles.container}>
-            <StatCard label="Total Stars" icon={require('../../assets/icons/star.png')} value={user.user_star} />
-            <StatCard label="Badges" icon={require('../../assets/icons/badge.png')} value={user.user_badge ?? 0} />
-        </View>
-         <FavoriteBadgesDisplay badges={badges} />
-        </View>
+export function StatsInventory({ user, badges }) {
+  if (!user) return null;
 
-    )
+  return (
+    <View>
+      <View style={styles.container}>
+        <StatCard label="Total Stars" icon={require('../../assets/icons/star.png')} value={user.user_star} />
+        <StatCard label="Badges" icon={require('../../assets/icons/badge.png')} value={user.user_badge ?? 0} />
+      </View>
+      <FavoriteBadgesDisplay badges={badges} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -58,16 +57,19 @@ const styles = StyleSheet.create({
 
   card: {
     width: 100,
-    height:70,
+    height: 53,
     borderRadius: 20,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
     alignItems: 'center',
+    justifyContent: 'center'
   },
 
   cardHeader: {
     width: '100%',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    paddingVertical: 4,
+    paddingVertical: 2,
     alignItems: 'center',
   },
 
@@ -76,12 +78,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 14,
   },
-  
+
   icon: {
     width: 22,
     height: 20,
   },
-  
+
   valueText: {
     fontSize: 16,
     fontWeight: 'bold',
