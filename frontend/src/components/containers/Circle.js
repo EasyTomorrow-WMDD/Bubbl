29d1c1176e1933d6206e6e46ac4e2111867eb93a
Andres Castaneda <yoac@Andress-MacBook-Air.local>
2025-06-29 01:04:57 -0700

    Child Main Screen Styles Updated

--- frontend/src/components/containers/Circle.js
+++ frontend/src/components/containers/Circle.js
@@ -2,44 +2,42 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Svg, { Circle } from 'react-native-svg';
+import {textColor, getCompleted, getTopicBg, getBgClass} from '../../styles/BubblColors'
 
-const CircularProgress = ({topic_number, completed}) => {
+const CircularProgress = ({ topic_number, completed, moduleNumber }) => {
   const radius = 30;
-  const strokeWidth = 10;
+  const strokeWidth = 7;
   const normalizedRadius = radius - strokeWidth / 2;
   const circumference = 2 * Math.PI * normalizedRadius;
-  const progress = completed; 
+  const progress = completed;
 
   const strokeDashoffset = circumference * (1 - progress);
 
-  return (
-    <View style={styles.wrapper}>
+return (
+    <View style={[styles.wrapper, { backgroundColor: getBgClass(moduleNumber) }]}>
       <Svg height={radius * 2} width={radius * 2}>
-        <Circle
-          stroke="gray"
-          fill="none"
-          cx={radius}
-          cy={radius}
-          r={normalizedRadius}
-          strokeWidth={strokeWidth}
-        />
-        <Circle
-          stroke="#75f0ff"
-          fill="none"
-          cx={radius}
-          cy={radius}
-          r={normalizedRadius}
-          strokeWidth={strokeWidth}
-          strokeDasharray={`${circumference} ${circumference}`}
-          strokeDashoffset={strokeDashoffset}
-          strokeLinecap="round"
-          rotation="-90"
-          origin={`${radius}, ${radius}`}
-        />
+        {/* This Validation Will Only Render The Circumference ring if it is completed */}
+        {completed > 0 && (
+          <Circle
+            stroke={getCompleted(moduleNumber)}
+            fill="none"
+            cx={radius}
+            cy={radius}
+            r={normalizedRadius}
+            strokeWidth={strokeWidth}
+            strokeDasharray={`${circumference} ${circumference}`}
+            strokeDashoffset={strokeDashoffset}
+            strokeLinecap="round"
+            rotation="-90"
+            origin={`${radius}, ${radius}`}
+          />
+        )}
       </Svg>
 
       <View style={styles.center}>
-        <Text style={styles.text}>{topic_number}</Text>
+        <Text style={[styles.text, { color: textColor(moduleNumber) }]}>
+          {topic_number}
+        </Text>
       </View>
     </View>
   );
@@ -49,14 +47,14 @@ export default CircularProgress;
 
 const styles = StyleSheet.create({
   wrapper: {
-    width: 30,
-    height: 30,
-    backgroundColor: '#000', 
+    width: 40,
+    height: 40,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 50,
-    marginLeft:15,
-    marginTop:20
+    marginLeft: 10,
+    marginTop: 10,
+
   },
   center: {
     position: 'absolute',
@@ -64,8 +62,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   text: {
-    fontSize: 28,
-    color: '#fff',
+    fontSize:22,
     fontWeight: 'bold',
   },
 });
