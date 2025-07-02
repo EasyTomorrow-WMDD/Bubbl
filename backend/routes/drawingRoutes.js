ed81a1129eedce2c30837b8830c3412ded885829
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-07-02 15:37:14 -0700

    Edit

--- backend/routes/drawingRoutes.js
+++ backend/routes/drawingRoutes.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const verifySupabaseToken = require('../middleware/verifySupabaseToken');
-// const drawingController = require('../controllers/drawingController');
-
-// router.post('/upload', verifySupabaseToken, drawingController.uploadDrawing);
-// router.get('/retrieve', verifySupabaseToken, drawingController.retrieveDrawings);
-
-// router.get('/:user_profile_id', verifySupabaseToken, drawingController.getDrawingsByUser);
-// router.get('/getByChild/:userId', verifySupabaseToken, drawingController.getDrawingsByChild);
-
-// module.exports = router;
-
-
-
 const express = require('express');
 const router = express.Router();
 
@@ -24,6 +8,5 @@ router.post('/upload', verifySupabaseToken, drawingController.uploadDrawing);
 
 router.get('/getByChild/:userId', verifySupabaseToken, drawingController.getDrawingsByChild);
 
-// router.get('/list/:userId', verifySupabaseToken, drawingController.listDrawings);
 
 module.exports = router;
