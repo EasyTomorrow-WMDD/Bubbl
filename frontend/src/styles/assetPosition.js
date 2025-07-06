895041f8734d75f27363da23f6bd46e477bed721
Andres Castaneda <andres.castaeda97@gmail.com>
2025-07-06 02:29:24 -0700

    Shop styles implemented

--- frontend/src/styles/assetPosition.js
+++ frontend/src/styles/assetPosition.js
@@ -2,27 +2,27 @@
 
 export const assetPositionMap = {
     // HATS
-    "red-hat": {
+    "Beannie": {
         top: 0,
         left: 60,
         width: 100,
         height: 80,
     },
-    "bow": {
+    "Bow": {
         top: -10,
         left: 190,
         width: 90,
         height: 85,
         transform: "rotate(-1deg)",
     },
-    "party": {
+    "Confetti": {
         top: -50,
         left: 200,
         width: 95,
         height: 90,
     },
 
-    "santa-hat": {
+    "Santa Hat": {
         top: 0,
         left: 60,
         width: 100,
