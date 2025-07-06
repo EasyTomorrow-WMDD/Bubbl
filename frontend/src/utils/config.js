38dda69abce51fb209303af1e3e172ed64394b33
Andres Castaneda <andres.castaeda97@gmail.com>
2025-07-05 19:57:52 -0700

    Comments delete for cleaning purpose

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,19 +1,9 @@
 const BASE_URL = 'http://bubbl.wmdd4950.com/app-backend';
 
 
-//  const BASE_URL = 'http://192.168.1.72:3000';
-//  const BASE_URL = 'http://10.100.2.107:3000';
-// const BASE_URL = 'http://192.168.1.72:3000';
-const BASE_URL = 'http://192.168.1.72:3000';
-// const BASE_URL = 'http://10.128.198.35:3000';
-// const BASE_URL = 'http://10.128.201.48:3000';
-
 const URI_URL = 'https://ogbfgllkkyehcurolhjf.supabase.co/storage/v1/object/public/assets/Badges'
 
 export { BASE_URL };
 export { URI_URL };
 
 
-// Langara 10.128.230.106
-// Langara-2 10.128.198.35
-// home 192.168.1.72
