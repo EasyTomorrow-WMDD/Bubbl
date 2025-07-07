7a7344db515128c1951f264aef6fc61fdd3a4174
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-07-07 13:06:16 -0700

    Update AuthWelcomeContainer.js

--- frontend/src/components/containers/AuthWelcomeContainer.js
+++ frontend/src/components/containers/AuthWelcomeContainer.js
@@ -1,42 +1,3 @@
-// import { View, Text, Image, TouchableOpacity,  ImageBackground } from 'react-native';
-// import { globalStyles } from '../../styles/BubblStyles';
-// import BubblButton from '../forms/BubblButton';
-// import DividerWithText from '../layout/DividerWithText';
-// import AuthLoginAnimation from './AuthLoginAnimationContainer';
-// import AuthLoginWhiteCard from './AuthLoginWhiteCardContainer';
-// import LottieView from 'lottie-react-native';
-
-// const AuthWelcomeContainer = ({navigation}) => {
-
-//   // This component serves as the welcome screen for authentication.
-//   // Here, users are navigating to either login or signup. There is no state management or logic for the moment. 
-
-//   // ==========================================================================
-//   // Render the welcome screen
-//   return (
-
-
-//     <ImageBackground
-//       source={require('../../assets/images/Login/Background.png')}
-//         style={globalStyles.backgroundLogin}
-//       // resizeMode="cover"
-//     >
-//       <View style={globalStyles.animationContainer}>
-//         <AuthLoginAnimation/>
-//       </View>
-
-//       <AuthLoginWhiteCard navigation={navigation} />
-
-//     </ImageBackground>
-//   );
-
-// };
-
-// export default AuthWelcomeContainer;
-
-
-
-
 import React, { useRef, useEffect, useState } from 'react';
 import { View, ImageBackground, StyleSheet } from 'react-native';
 import { globalStyles } from '../../styles/BubblStyles';
@@ -47,6 +8,11 @@ import LottieView from 'lottie-react-native';
 const AuthWelcomeContainer = ({ navigation }) => {
   const animationRef = useRef(null);
   const [animationFinished, setAnimationFinished] = useState(false);
+  
+//   // This component serves as the welcome screen for authentication.
+//   // Here, users are navigating to either login or signup. There is no state management or logic for the moment. 
+
+//   // ==========================================================================
 
   useEffect(() => {
     if (animationRef.current) {
@@ -61,6 +27,8 @@ const AuthWelcomeContainer = ({ navigation }) => {
         source={require('../../assets/images/Login/Background.png')}
         style={globalStyles.backgroundLogin}
       >
+
+//   // Render the welcome screen
         <View style={globalStyles.animationContainer}>
           <AuthLoginAnimation />
         </View>
