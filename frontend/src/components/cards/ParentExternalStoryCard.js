f078f97ea8b6a1cddfedf67ebad5d707a6313eb4
Yasuhiro Hasegawa <yasuhiro.hasegawa@gmail.com>
2025-07-08 08:31:18 -0700

    EZ-175 beta development on profile and parent screens part 1

---
+++ frontend/src/components/cards/ParentExternalStoryCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import placeholderImage from '../../assets/images/placeholder_parent_stories.png'; // placeholder image for stories if featured image is not available
+import BubblColors from '../../styles/BubblColors';
+import { fontStyles } from '../../styles/BubblFontStyles';
+import { parentStyles } from '../../styles/BubblParentMainStyles';
+
+// ============================================================================
+// ParentExternalStoryCard Component
+const ParentExternalStoryCard = ({ article }) => {
+
+  // ----------------------------------------------------------------
+  // Method to handle article press
+  const onPress = () => {
+    if (article.url) {
+      Linking.openURL(article.url).catch(err => console.error('Failed to open URL:', err));
+    }
+  };
+
+  // ----------------------------------------------------------------
+  // Render the external story card
+  return (
+    <TouchableOpacity style={parentStyles.parentStoryOtherCardContainer} onPress={onPress}>
+
+      {/* Card image & tag */}
+      <View style={parentStyles.parentStoryOtherCardImageContainer}>
+        {/* Image */}
+        <Image
+          source={article.urlToImage ? { uri: article.urlToImage } : placeholderImage}
+          style={parentStyles.parentStoryOtherCardImage}
+          resizeMode="cover"
+        />
+        {/* Tag */}
+        <View style={parentStyles.parentStoryOtherCardReadTag}>
+          <Text style={[fontStyles.bodySmall, parentStyles.parentStoryOtherCardReadTagText]}>
+            External
+          </Text>
+        </View>
+      </View>
+      {/* Card text */}
+      <View style={parentStyles.parentStoryOtherCardTextContainer}>
+        <Text style={[fontStyles.heading2, parentStyles.parentStoryOtherCardTitle]}>{article.title}</Text>
+        <Text style={[fontStyles.bodyDefault, parentStyles.parentStoryOtherCardSummary]}>{article.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ParentExternalStoryCard;
---
+++ frontend/src/utils/AvatarMappings.js
@@ -0,0 +1,8 @@
+// Avatar Mapping: 
+// Mapping of the "avatar_id" in the user record vs actual file path+name in the assets folder.
+export const avatarImages = {
+  'avatar01' : require('../assets/avatars/avatar01.png'),
+  'avatar02' : require('../assets/avatars/avatar02.png'),
+  'avatar03' : require('../assets/avatars/avatar03.png'),
+  'avatar04' : require('../assets/avatars/avatar04.png'),
+};
