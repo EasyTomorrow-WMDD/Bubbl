7619cf35b68d86e763096f1d4701dc99e7c933ea
Andres Castaneda <andres.castaeda97@gmail.com>
2025-07-09 13:11:56 -0700

    Try 10000 to fix PAINT.PNG bug

--- frontend/src/components/layout/ChildNavbar.js
+++ frontend/src/components/layout/ChildNavbar.js
@@ -36,7 +36,7 @@ const ChildNavbar = ({ navigation, childProfileId }) => {
       >
         <View style={[activeTab === 'mood draw' ? styles.iconWrapperActive : styles.iconWrapper]}>
           <Image
-            source={require('../../assets/icons/Paint.png')}
+            source={require('../../assets/icons/paint.png')}
             style={styles.navIcon}
             resizeMode="contain"
           />
