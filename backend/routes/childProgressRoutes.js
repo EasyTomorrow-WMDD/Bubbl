9a55a03c1b3ed06d6277f52e2feb9b44db5a3b49
Andres Castaneda <andres.castaeda97@gmail.com>
2025-07-16 12:44:40 -0700

    Fixing shadow position

--- backend/routes/childProgressRoutes.js
+++ backend/routes/childProgressRoutes.js
@@ -9,7 +9,7 @@ router.get('/userProgress/:userId', controller.getChildProgress);
 router.post('/saveProgress', controller.saveProgress);
 router.get('/userAvatar/:userId', controller.getChildAvatar);
 router.post('/saveDrawingProgress', controller.saveDrawingProgress);
-router.post('/levels', controller.getRefLevel);
+router.get('/levels', controller.getRefLevel);
 // router.post('/updateUserLevel', controller.updateUserLevel);
 
 
