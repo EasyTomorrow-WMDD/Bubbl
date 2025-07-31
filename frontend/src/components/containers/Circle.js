c60d0b43e0679ba51769a6b5075b7ec65728054b
Jaime Herrera <jaimeherrerazuniga@gmail.com>
2025-07-31 12:51:10 -0700

    Android Bug Stroke Child Module Number

--- frontend/src/components/containers/Circle.js
+++ frontend/src/components/containers/Circle.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import Svg, { Circle } from 'react-native-svg';
 import {textColor, getCompleted, getTopicBg, getBgClass} from '../../styles/BubblColors'
 import { fontStyles } from '../../styles/BubblFontStyles';
@@ -15,32 +15,35 @@ const CircularProgress = ({ topic_number, completed, moduleNumber }) => {
   const strokeDashoffset = circumference * (1 - progress);
 
 return (
-    <View style={[styles.wrapper, { backgroundColor: getBgClass(moduleNumber) }]}>
-      <Svg height={radius * 2} width={radius * 2}>
-        {/* This Validation Will Only Render The Circumference ring if it is completed */}
-        {completed > 0 && (
-          <Circle
-            stroke={getCompleted(moduleNumber)}
-            fill="none"
-            cx={radius}
-            cy={radius}
-            r={normalizedRadius}
-            strokeWidth={strokeWidth}
-            strokeDasharray={`${circumference} ${circumference}`}
-            strokeDashoffset={strokeDashoffset}
-            strokeLinecap="round"
-            rotation="-90"
-            origin={`${radius}, ${radius}`}
-          />
-        )}
-      </Svg>
+<View style={[styles.wrapper, { backgroundColor: getBgClass(moduleNumber) }]}>
+  <Svg
+    height={radius * 2}
+    width={radius * 2}
+    style={Platform.OS === 'android' ? { marginTop: 13, marginLeft: 13 } : null}
+  >
+    {completed > 0 && (
+      <Circle
+        stroke={getCompleted(moduleNumber)}
+        fill="none"
+        cx={radius}
+        cy={radius}
+        r={normalizedRadius}
+        strokeWidth={strokeWidth}
+        strokeDasharray={`${circumference} ${circumference}`}
+        strokeDashoffset={strokeDashoffset}
+        strokeLinecap="round"
+        rotation="-90"
+        origin={`${radius}, ${radius}`}
+      />
+    )}
+  </Svg>
 
-      <View style={styles.center}>
-        <Text style={[fontStyles.heading1, { color: textColor(moduleNumber) }]}>
-          {topic_number}
-        </Text>
-      </View>
-    </View>
+  <View style={styles.center}>
+    <Text style={[fontStyles.heading1, { color: textColor(moduleNumber) }]}>
+      {topic_number}
+    </Text>
+  </View>
+</View>
   );
 };
 
